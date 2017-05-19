ea7dde80c37408edeacc9be4a89ac01e9b2235f2
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-19 15:09:34 -0400

    Getting closer with implementations for part1

--- src/main/java/zygmundfelt/dan/mesolabstrings/PADriversLicense.java
+++ src/main/java/zygmundfelt/dan/mesolabstrings/PADriversLicense.java
@@ -1,5 +1,6 @@
 package zygmundfelt.dan.mesolabstrings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PADriversLicense {
@@ -13,15 +14,15 @@ public class PADriversLicense {
     private String state;
     private String zipCode;
     private String dateOfBirth;
-    private String sex;
-    private String licenseClass;
-    private String eyes;
-    private String height;
     private String issued;
     private String expires;
+    private String sex;
+    private String eyes;
+    private String height;
     private String organDonor;
+    private String licenseClass;
 
-    public PADriversLicense(String licenseNumber, String lastName, String firstName, String middleName, String address, String city, String state, String zipCode, String dateOfBirth, String sex, String licenseClass, String eyes, String height, String issued, String expires, String organDonor) {
+    public PADriversLicense(String licenseNumber, String lastName, String firstName, String middleName, String address, String city, String state, String zipCode, String dateOfBirth, String issued, String expires, String sex, String eyes, String height, String organDonor, String licenseClass) {
         this.licenseNumber = licenseNumber;
         this.lastName = lastName;
         this.firstName = firstName;
@@ -31,27 +32,30 @@ public class PADriversLicense {
         this.state = state;
         this.zipCode = zipCode;
         this.dateOfBirth = dateOfBirth;
-        this.sex = sex;
-        this.licenseClass = licenseClass;
-        this.eyes = eyes;
-        this.height = height;
         this.issued = issued;
         this.expires = expires;
+        this.sex = sex;
+        this.eyes = eyes;
+        this.height = height;
         this.organDonor = organDonor;
+        this.licenseClass = licenseClass;
     }
 
-    //TODO
+    //TODO - check input to ensure valid header
     public static List<PADriversLicense> deserialize(String input) {
-        return null;
+        String[] arr = input.split(",");
+        List<PADriversLicense> list = new ArrayList<PADriversLicense>();
+        for(int i = 1; i < arr.length / 16; i++) {
+            list.add(new PADriversLicense(arr[i*16],arr[i*16 + 1],arr[i*16 + 2],arr[i*16+3],arr[i*16+4],arr[i*16+5],arr[i*16 + 6],arr[i*16 + 7],arr[i*16 + 8],arr[i*16 + 9],arr[i*16 + 10],arr[i*16 + 11],arr[i*16 + 12],arr[i*16 + 13],arr[i*16 + 14],arr[i*16 + 15]));
+        }
+        return list;
     }
 
-    //TODO
     public static String getCSVHeader() {
-        return "licenseNumber,lastName,firstName,middleName,address,city,state,zipCode,dateOfBirth,issued,expires,sex,eyes,height,organDonor,licenseClass";
+        return "licenseNumber,lastName,firstName,middleName,address,city,state,zipCode,dateOfBirth,issued,expires,sex,eyes,height,organDonor,licenseClass,";
     }
 
-    //TODO
-    public String serializeToCSV(List<PADriversLicense> list) {
+    public static String serializeToCSV(List<PADriversLicense> list) {
         StringBuilder sb = new StringBuilder();
         sb.append(getCSVHeader());
         for(PADriversLicense license : list) {
--- src/test/java/zygmundfelt/dan/mesolabstrings/TestPADriversLicense.java
+++ src/test/java/zygmundfelt/dan/mesolabstrings/TestPADriversLicense.java
@@ -2,17 +2,33 @@ package zygmundfelt.dan.mesolabstrings;
 
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestPADriversLicense {
 
     @Test
     public void getCSVHeaderTest() {
-        String expected = "licenseNumber,lastName,firstName,middleName,address,city,state,zipCode,dateOfBirth,issued,expires,sex,eyes,height,organDonor,licenseClass";
+        String expected = "licenseNumber,lastName,firstName,middleName,address,city,state,zipCode,dateOfBirth,issued,expires,sex,eyes,height,organDonor,licenseClass,";
 
         String actual = PADriversLicense.getCSVHeader();
 
         Assert.assertEquals(expected, actual);
     }
 
-    
+    @Test
+    public void serializeToCSVSingleRecordTest() {
+        List<PADriversLicense> list = new ArrayList<PADriversLicense>();
+        list.add(new PADriversLicense("001","Zygmund-Felt","Dan","C","123 Skidoo Ln","philadelphia","PA","19020","09/09/2009","12/12/2012", "12/12/2018","M","Brown","5'8","Yes", "D"));
+        String expected = "licenseNumber,lastName,firstName,middleName,address,city,state,zipCode,dateOfBirth,issued,expires,sex,eyes,height,organDonor,licenseClass,001,Zygmund-Felt,Dan,C,123 Skidoo Ln,philadelphia,PA,19020,09/09/2009,12/12/2012,12/12/2018,M,Brown,5'8,Yes,D";
 
+        String actual = PADriversLicense.serializeToCSV(list);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deserializeTest() {
+
+    }
 }
